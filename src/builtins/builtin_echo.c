bfb736c70d5f2ae706d0fe0edc9da973fe601131
araggohnxd <matheusnagato300@gmail.com>
2022-07-22 15:34:53 -0300

    add: done echo builtin

---
+++ src/builtins/builtin_echo.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_echo.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/22 13:08:57 by maolivei          #+#    #+#             */
+/*   Updated: 2022/07/22 13:43:07 by maolivei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	builtin_echo(t_minishell *data, int index, t_bool is_child)
+{
+	size_t	i;
+	t_bool	newline;
+
+	i = 1;
+	newline = TRUE;
+	if (data->cmd.args[index][i] && !ft_strcmp(data->cmd.args[index][i], "-n"))
+	{
+		newline = FALSE;
+		++i;
+	}
+	while (data->cmd.args[index][i])
+	{
+		ft_putstr_fd(data->cmd.args[index][i++], STDOUT_FILENO);
+		if (data->cmd.args[index][i])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+	}
+	if (newline)
+		ft_putendl_fd("", STDOUT_FILENO);
+	if (is_child)
+		exit_free(data, EXIT_SUCCESS);
+	return (TRUE);
+}
--- src/builtins/builtin_utils.c
+++ src/builtins/builtin_utils.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 14:07:33 by maolivei          #+#    #+#             */
-/*   Updated: 2022/07/22 13:15:43 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/07/22 15:28:31 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,13 @@ int	check_builtin(t_minishell *data, int index, t_bool is_child)
 {
 	if (ft_strcmp(*data->cmd.args[index], "exit") == 0)
 		return (builtin_exit(data, index, is_child));
+	else if (ft_strcmp(*data->cmd.args[index], "echo") == 0)
+		return (builtin_echo(data, index, is_child));
 	return (FALSE);
 }
 
 int	is_builtin(char *cmd)
 {
-	return (ft_strcmp(cmd, "exit") == 0);
+	return (ft_strcmp(cmd, "exit") == 0
+		|| ft_strcmp(cmd, "echo") == 0);
 }
