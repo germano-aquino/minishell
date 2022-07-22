b2bfc052675ef7e0aab342b2dc7410c90b4b0bcf
araggohnxd <matheusnagato300@gmail.com>
2022-07-22 13:17:05 -0300

    fix: check for proper args index in builtin exit

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 13:20:53 by maolivei          #+#    #+#             */
-/*   Updated: 2022/07/22 12:58:36 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:12:50 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,27 @@ static int	out_llong_range(char *str)
 	return (FALSE);
 }
 
-static void	numeric_argument_required(t_minishell *data)
+static void	numeric_argument_required(t_minishell *data, int index)
 {
 	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-	ft_putstr_fd(data->cmd.args[0][1], STDERR_FILENO);
+	ft_putstr_fd(data->cmd.args[index][1], STDERR_FILENO);
 	ft_putendl_fd(": numeric argument required", STDERR_FILENO);
 	exit_free(data, 2);
 }
 
-int	builtin_exit(t_minishell *data, t_bool is_child)
+int	builtin_exit(t_minishell *data, int index, t_bool is_child)
 {
 	t_llong	exit_code;
 
 	exit_code = 0;
 	if (data->cmd.cmds_amount == 1)
 		ft_putendl_fd("exit", STDOUT_FILENO);
-	if (data->cmd.args[0][1])
+	if (data->cmd.args[index][1])
 	{
-		if (out_llong_range(data->cmd.args[0][1])
-			|| !ft_is_number_str(data->cmd.args[0][1]))
-			numeric_argument_required(data);
-		else if (data->cmd.args[0][2])
+		if (out_llong_range(data->cmd.args[index][1])
+			|| !ft_is_number_str(data->cmd.args[index][1]))
+			numeric_argument_required(data, index);
+		else if (data->cmd.args[index][2])
 		{
 			ft_putendl_fd("minishell: exit: too many arguments", STDERR_FILENO);
 			if (is_child)
@@ -58,7 +58,7 @@ int	builtin_exit(t_minishell *data, t_bool is_child)
 			return (TRUE);
 		}
 		else
-			exit_code = ft_atoll(data->cmd.args[0][1]);
+			exit_code = ft_atoll(data->cmd.args[index][1]);
 	}
 	exit_free(data, exit_code);
 	return (TRUE);
--- src/builtins/builtin_utils.c
+++ src/builtins/builtin_utils.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 14:07:33 by maolivei          #+#    #+#             */
-/*   Updated: 2022/07/22 13:02:51 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:15:43 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int	ft_is_number_str(const char *str)
 int	check_builtin(t_minishell *data, int index, t_bool is_child)
 {
 	if (ft_strcmp(*data->cmd.args[index], "exit") == 0)
-		return (builtin_exit(data, is_child));
+		return (builtin_exit(data, index, is_child));
 	return (FALSE);
 }
 
