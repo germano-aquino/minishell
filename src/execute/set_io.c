30013bce872901d6370d55f0e3e7e0250375c405
araggohnxd <matheusnagato300@gmail.com>
2022-09-16 23:14:26 -0300

    chore: improve readability in set_io

--- src/execute/set_io.c
+++ src/execute/set_io.c
@@ -6,48 +6,45 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 17:17:41 by maolivei          #+#    #+#             */
-/*   Updated: 2022/09/16 17:17:46 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/09/16 23:11:16 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	set_conditional_fd(t_minishell *data, t_workspace *vars, int index)
+static void	set_output_files(t_minishell *data, t_workspace *vars, int i)
 {
-	if (data->cmd.files[index].which_input == IN_INFILE \
-	|| data->cmd.files[index].which_input == IN_HEREDOC)
-		dup42(open(data->cmd.files[index].infile, O_RDONLY),
-			vars->fd[index][IN]);
-	else if (index == 0 || data->cmd.connector[index - 1] != PIPE)
-		dup42(dup(STDIN), vars->fd[index][IN]);
-	if (data->cmd.files[index].which_output == OUT_TRUNC)
-		dup42(open(data->cmd.files[index].outfile, \
-			O_WRONLY | O_CREAT | O_TRUNC, 0644), vars->fd[index][OUT]);
-	else if (data->cmd.files[index].which_output == OUT_APPEND)
-		dup42(open(data->cmd.files[index].outfile, \
-			O_WRONLY | O_CREAT | O_APPEND, 0644), vars->fd[index][OUT]);
-	else if (index == data->cmd.cmds_amount - 1 \
-	|| data->cmd.connector[index] != PIPE)
-		dup42(dup(STDOUT), vars->fd[index][OUT]);
+	const t_output	current_output = data->cmd.files[i].which_output;
+	const char		*file_out = data->cmd.files[i].outfile;
+	int				outfile_fd;
+
+	if (current_output == OUT_STDOUT && \
+	(i == data->cmd.cmds_amount - 1 || data->cmd.connector[i] != PIPE))
+		outfile_fd = dup(STDOUT);
+	else if (current_output == OUT_TRUNC)
+		outfile_fd = open(file_out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (current_output == OUT_APPEND)
+		outfile_fd = open(file_out, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else
+		return ;
+	dup42(outfile_fd, vars->fd[i][OUT]);
 }
 
-static void	set_piped_fd(t_minishell *data, t_workspace *vars, int index)
+static void	set_input_files(t_minishell *data, t_workspace *vars, int i)
 {
-	if (data->cmd.files[index].which_input == IN_STDIN && index == 0)
-		dup42(dup(STDIN), vars->fd[index][IN]);
-	else if (data->cmd.files[index].which_input == IN_INFILE \
-	|| data->cmd.files[index].which_input == IN_HEREDOC)
-		dup42(open(data->cmd.files[index].infile, O_RDONLY),
-			vars->fd[index][IN]);
-	if (data->cmd.files[index].which_output == OUT_STDOUT \
-	&& index == data->cmd.cmds_amount - 1)
-		dup42(dup(STDOUT), vars->fd[index][OUT]);
-	else if (data->cmd.files[index].which_output == OUT_TRUNC)
-		dup42(open(data->cmd.files[index].outfile, \
-			O_WRONLY | O_CREAT | O_TRUNC, 0644), vars->fd[index][OUT]);
-	else if (data->cmd.files[index].which_output == OUT_APPEND)
-		dup42(open(data->cmd.files[index].outfile, \
-			O_WRONLY | O_CREAT | O_APPEND, 0644), vars->fd[index][OUT]);
+	const t_input	current_input = data->cmd.files[i].which_input;
+	const char		*file_in = data->cmd.files[i].infile;
+	int				infile_fd;
+
+	if (i == 0 && current_input == IN_STDIN)
+		infile_fd = dup(STDIN);
+	else if (current_input == IN_STDIN && data->cmd.connector[i - 1] != PIPE)
+		infile_fd = dup(STDIN);
+	else if (current_input == IN_INFILE || current_input == IN_HEREDOC)
+		infile_fd = open(file_in, O_RDONLY);
+	else
+		return ;
+	dup42(infile_fd, vars->fd[i][IN]);
 }
 
 void	set_input_output_fd(t_minishell *data, t_workspace *vars)
@@ -57,10 +54,7 @@ void	set_input_output_fd(t_minishell *data, t_workspace *vars)
 	index = -1;
 	while (++index < data->cmd.cmds_amount)
 	{
-		if (data->cmd.connector[index] == PIPE \
-		|| data->cmd.connector[index] == NONE)
-			set_piped_fd(data, vars, index);
-		else
-			set_conditional_fd(data, vars, index);
+		set_input_files(data, vars, index);
+		set_output_files(data, vars, index);
 	}
 }
