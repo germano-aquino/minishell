c305392449218a90eec4f53a7d0f07fbec6085b0
maolivei <maolivei@student.42sp.org.br>
2022-09-17 07:46:20 +0200

    fix: compiling error in workspaces

--- src/utils/syntax_error.c
+++ src/utils/syntax_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   syntax_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 14:59:00 by maolivei          #+#    #+#             */
-/*   Updated: 2022/09/16 14:59:13 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/09/17 07:44:11 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,29 +24,26 @@ static void	word_syntax_error(t_minishell *data, t_node *input)
 
 void	syntax_error(t_minishell *data, t_node *input)
 {
-	char	*error;
-
 	if (!input)
-		error = ft_strjoin(ERR_SYNTAX, NEWLINE_STR);
+		redisplay_prompt(data, ERR_SYNTAX NEWLINE_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_REDIR_TRUNC)
-		error = ft_strjoin(ERR_SYNTAX, TRUNC_STR);
+		redisplay_prompt(data, ERR_SYNTAX TRUNC_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_REDIR_APPEND)
-		error = ft_strjoin(ERR_SYNTAX, APPEND_STR);
+		redisplay_prompt(data, ERR_SYNTAX APPEND_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_REDIR_INFILE)
-		error = ft_strjoin(ERR_SYNTAX, INPUT_STR);
+		redisplay_prompt(data, ERR_SYNTAX INPUT_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_REDIR_HEREDOC)
-		error = ft_strjoin(ERR_SYNTAX, HEREDOC_STR);
+		redisplay_prompt(data, ERR_SYNTAX HEREDOC_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_PIPE)
-		error = ft_strjoin(ERR_SYNTAX, PIPE_STR);
+		redisplay_prompt(data, ERR_SYNTAX PIPE_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_OR)
-		error = ft_strjoin(ERR_SYNTAX, OR_STR);
+		redisplay_prompt(data, ERR_SYNTAX OR_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_AND)
-		error = ft_strjoin(ERR_SYNTAX, AND_STR);
+		redisplay_prompt(data, ERR_SYNTAX AND_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_OPEN_PARENTHESIS)
-		error = ft_strjoin(ERR_SYNTAX, OPEN_STR);
+		redisplay_prompt(data, ERR_SYNTAX OPEN_STR, NULL, EXIT_BAD_USAGE);
 	else if (input->tok == TOK_CLOSE_PARENTHESIS)
-		error = ft_strjoin(ERR_SYNTAX, CLOSE_STR);
+		redisplay_prompt(data, ERR_SYNTAX CLOSE_STR, NULL, EXIT_BAD_USAGE);
 	else
 		word_syntax_error(data, input);
-	redisplay_prompt(data, error, error, EXIT_BAD_USAGE);
 }
